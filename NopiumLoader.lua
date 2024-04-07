6901e8fcfbedf6889ce8ad9eea7005e927e40711
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2024-04-07 15:09:18 -0500

    Update NopiumLoader.lua

--- NopiumLoader.lua
+++ NopiumLoader.lua
@@ -1,1 +1,7 @@
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/NopiumV2./main/stats_file.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/NopiumV2./main/stats_file.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/NopiumV2./main/stats_file.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/NopiumV2./main/stats_file.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/NopiumV2./main/stats_file.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/NopiumV2./main/stats_file.lua"))()
