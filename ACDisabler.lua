687cade2b327e55088e95f452d3afbd9f7417d19
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2024-04-07 14:59:08 -0500

    Delete ACDisabler.lua

--- ACDisabler.lua
+++
@@ -1,7 +0,0 @@
-repeat task.wait()
-    game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("RocketImpulse"):InvokeServer(unpack({
-        [1] = {
-            ["velocity"] = Vector3.new(math.huge, math.huge, math.huge)
-        }
-    }))
-until false
