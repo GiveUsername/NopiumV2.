618eb42ab79acf1baf439a546c691d7736275ff7
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2024-04-09 16:47:51 -0500

    Update and rename NopiumLoader.lua to NopiumUpdate.lua

---
+++ NopiumUpdate.lua
@@ -0,0 +1,9 @@
+local update = false
+local discord = "discord.gg/whNQmrJS9F"
+local text = "Please Update your Nopium! | " .. discord
+if update then
+    game.Players.LocalPlayer:Kick(text)
+	  warn(text)
+else
+    print("No updates found)
+end
