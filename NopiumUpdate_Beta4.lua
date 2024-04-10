8af4b0fc150e5afe2fa744d20f046e722b14e9b3
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2024-04-09 19:02:49 -0500

    Update NopiumUpdate_Beta4.lua

--- NopiumUpdate_Beta4.lua
+++ NopiumUpdate_Beta4.lua
@@ -2,8 +2,8 @@ local update = false
 local discord = "discord.gg/whNQmrJS9F"
 local text = "Please Update your Nopium! | " .. discord
 if update then
-    game.Players.LocalPlayer:Kick(text)
-	  warn(text)
+	game.Players.LocalPlayer:Kick(text)
+	warn(text)
 else
     print("No updates found")
 end
